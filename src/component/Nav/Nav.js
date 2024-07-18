d04e94d9a2a877b74c738a9dd1c10c577112c9a1
Ildar_Zabirof <uldar82@gmail.com>
2024-07-19 00:08:07 +0500

    asd

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Logo from '../../assets/logo.png';
 import { GiSmartphone } from "react-icons/gi";
 import { FaSearch } from "react-icons/fa";
@@ -16,49 +16,62 @@ function Nav() {
             setUserScrolling(true);
         }
     });
-    console.log(userScrolling)
-    return (
 
-            <nav className={userScrolling? "nav active" : "nav"}>
+    // useEffect(() => {
+    //     document.querySelector('.none').remove()
+    // }, []);
+    return (
+        <>
+            <nav className={userScrolling ? "nav none" : "nav"}>
                 <div className={'container__nav'}>
                     <div className="left">
                         <img src={Logo} alt=""/>
-                        {userScrolling ? '' : (
-                            <span className={'left__text'}>Разработано в Германии. Сделано в России.</span>
-
-                        )}
                         <ul className="nav__list">
-                            <li className="nav__item">О комнании</li>
-                            <li className="nav__item">О продукции</li>
-                            <li className="nav__item">Каталог товаров</li>
+                            <li className={userScrolling ? "nav__item " : "nav__item"}>О комнании</li>
+                            <li className={userScrolling ? "nav__item " : "nav__item"}>О продукции</li>
+                            <li className={userScrolling ? "nav__item " : "nav__item"}>Каталог товаров</li>
                         </ul>
                     </div>
                     <div className="right">
-                        {userScrolling ?
-                            <>
-                                <GiSmartphone className={'telephone__icon'}/><span>+7 (905) 543 23 45</span>
-                                <div className={'nav__button basket'}>
-                                    <FaShoppingBasket/><span className={'basket__text'}>4290 р.</span>
-                                </div>
+
+                        <GiSmartphone className={'telephone__icon'}/><span>+7 (905) 543 23 45</span>
+                        <div className={'nav__button basket'}>
+                            <FaShoppingBasket/><span className={'basket__text'}>4290 р.</span>
+                        </div>
+
+
+                        <FaUser className={'nav__button'}/>
+                        <FaSearch className={'nav__button'}/>
+
+                    </div>
+                </div>
+
+            </nav>
+            <nav className={userScrolling ? "nav nav2 active" : "nav nav2 "}>
+                <div className={'container__nav'}>
+                    <div className="left">
+                        <img src={Logo} alt=""/>
+
+                        <span className={'left__text'}>Разработано в Германии. Сделано в России.</span>
+
+                    </div>
+                    <div className="right">
+
+
                                 <FaUser className={'nav__button'}/>
                                 <FaSearch className={'nav__button'}/>
-                            </>
 
-                            :
-                            <>
-                                <FaUser className={'nav__button'}/>
-                                <FaSearch className={'nav__button'}/>
-                            </>
-
-                        }
 
                     </div>
                 </div>
 
             </nav>
 
+        </>
+
 
     );
+
 }
 
 export default Nav;
